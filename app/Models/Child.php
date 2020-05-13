9b8aeba25afd7f2d0febbc125c4f9d4e3668cf2b
QuangNgocHung <ngochung.quang.1310@gmail.com>
2020-05-13 23:40:30 +0700

    be-management-parent (Add teamtnt/laravel-scout-tntsearch-driver package)

--- app/Models/Child.php
+++ app/Models/Child.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Arr;
+use Laravel\Scout\Searchable;
+
 class Child extends Model
 {
+    use Searchable;
+
     protected $table = 'children';
+
+    /**
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $this->loadMissing([
+            'user'
+        ]);
+
+        return Arr::only(Arr::dot($this->toArray()), [
+            'id',
+            'user.full_name',
+            'user.email',
+            'user.phone_number',
+        ]);
+    }
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
