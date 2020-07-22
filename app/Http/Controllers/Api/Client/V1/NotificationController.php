b70c7a5f5230e53c8d26a1cb00f1d88d1d95cc62
Quang Hung <ngochung.quang.1310@gmail.com>
2020-07-22 10:15:47 +0700

    update

--- app/Http/Controllers/Api/Client/V1/NotificationController.php
+++ app/Http/Controllers/Api/Client/V1/NotificationController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Api\Client\V1;
 
 use App\Http\Controllers\Api\Client\Controller;
-use App\Models\FCMToken;
 use App\Notifications\ChildRemind;
-use Illuminate\Http\Response;
 
 class NotificationController extends Controller
 {
@@ -19,13 +17,6 @@ class NotificationController extends Controller
         $title = $this->request()->get('title');
         $message = $this->request()->get('message');
 
-        $fcmToken = FCMToken::whereUserId($this->user()->id)
-            ->first();
-
-        if (empty($fcmToken)) {
-            abort(Response::HTTP_BAD_REQUEST, "Not found fcm token of user");
-        }
-
-        $this->user()->notifyNow(new ChildRemind($title, $message));
+        $this->user()->notify(new ChildRemind($title, $message));
     }
 }
